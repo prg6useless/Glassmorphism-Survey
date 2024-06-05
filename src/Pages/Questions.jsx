e15269864593cdacf7bace9db0c0e98320451d87
Saral Sainju <sainju.saral433@gmail.com>
2024-06-05 12:45:23 +0545

    home page

--- src/Pages/Questions.jsx
+++ src/Pages/Questions.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase"; //
 import { questions } from "../Pages/data";
@@ -8,6 +8,12 @@ const Questions = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const { name, email } = location.state || {};
+
+  useEffect(() => {
+    if (!name || !email) {
+      navigate("/home");
+    }
+  }, [name, email, navigate]);
 
   const [selectedOptions, setSelectedOptions] = useState({});
   const [errors, setErrors] = useState({});
@@ -64,13 +70,31 @@ const Questions = () => {
         console.error("Error adding document: ", e);
       }
       console.log("Selected Options:", answers);
-      navigate("/sliders");
+      navigate("/sliders", { replace: true });
     }
   };
 
   return (
     <>
-      {" "}
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+        <div
+          className="relative right-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] translate-x-1/2 -rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:right-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>{" "}
       <form
         action="#"
         className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
@@ -139,6 +163,9 @@ const Questions = () => {
             </div>
           );
         })}
+        <h4 className=" mt-4 max-w-md text-left font-bold text-lg">
+          The form cannot be resubmitted once you go to the next page
+        </h4>
         <button
           type="submit"
           className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
--- src/Routes/AllRoute.jsx
+++ src/Routes/AllRoute.jsx
@@ -3,12 +3,14 @@ import Email from "../Pages/Email";
 import Questions from "../Pages/Questions";
 import Sliders from "../Pages/Sliders";
 import ThankYou from "../Pages/ThankYou";
+import Home from "../Pages/Home";
 
 const AllRoute = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/email" />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/email" element={<Email />} />
         <Route path="/questions" element={<Questions />} />
         <Route path="/sliders" element={<Sliders />} />
