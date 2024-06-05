7e2b96fa7f1d42c59c9a12eb46a4add153c7f20a
Saral Sainju <sainju.saral433@gmail.com>
2024-06-05 13:20:47 +0545

    firebase edit

--- src/firebase.js
+++ src/firebase.js
@@ -16,7 +16,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Export firestore database
